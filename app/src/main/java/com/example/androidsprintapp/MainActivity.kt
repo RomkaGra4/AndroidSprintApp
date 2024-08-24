854f290ecc3c26439563fa6682bbfd50cbcd5c03
romka <romka_gra4@mail.ru>
2024-08-25 02:46:27 +0500

    RA-03: With second corrections

--- app/src/main/java/com/example/androidsprintapp/MainActivity.kt
+++ app/src/main/java/com/example/androidsprintapp/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.example.androidsprintapp
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.androidsprintapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
     }
 }
