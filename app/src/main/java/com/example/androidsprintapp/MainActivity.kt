454ffb0e981b229a383d9ebdb82968453f0df74a
romka <romka_gra4@mail.ru>
2024-09-22 22:02:58 +0500

    RA-05: Completed 3 points

--- app/src/main/java/com/example/androidsprintapp/MainActivity.kt
+++ app/src/main/java/com/example/androidsprintapp/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.androidsprintapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.example.androidsprintapp.databinding.ActivityMainBinding
 
 
@@ -18,6 +21,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val categoriesButton = findViewById<Button>(R.id.categories_button)
+        val favoriteButton = findViewById<Button>(R.id.favorite_button)
+
+        categoriesButton.setOnClickListener {
+            supportFragmentManager.commit {
+                replace<CategoriesListFragment>(R.id.categories_button)
+                setReorderingAllowed(true)
+                addToBackStack("")
+            }
+        }
+
+        favoriteButton.setOnClickListener {
+            supportFragmentManager.commit {
+                replace<FavoritesFragment>(R.id.favorite_button)
+                setReorderingAllowed(true)
+                addToBackStack("")
+            }
+        }
 
         supportFragmentManager.commit {
             setReorderingAllowed(true)
